d8fc779721de541b983f8824c43c52ca8e81cb4b
den Drown <den@dendrown.net>
2017-07-19 23:14:29 -0400

    License and headers for sila-weka

--- clojure/sila-weka/project.clj
+++ clojure/sila-weka/project.clj
@@ -1,6 +1,6 @@
 (defproject sila-weka "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :description "Bridge to Weka ML utilities for sila"
+  :url "http://dendrown.net"
+  :license {:name "BSD 3-clause License"
+            :url "https://spdx.org/licenses/BSD-3-Clause.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
--- clojure/sila-weka/src/sila_weka/core.clj
+++ clojure/sila-weka/src/sila_weka/core.clj
@@ -1,3 +1,15 @@
+;;;-------------------------------------------------------------------
+;;;
+;;;        _/_/_/  _/_/_/  _/          _/_/
+;;;     _/          _/    _/        _/    _/
+;;;      _/_/      _/    _/        _/_/_/_/
+;;;         _/    _/    _/        _/    _/
+;;;  _/_/_/    _/_/_/  _/_/_/_/  _/    _/
+;;;
+;;; Emotion Mining and Machine Learning for Climate Change communication
+;;;
+;;; @copyright 2017 Dennis Drown et l'Université du Québec à Montréal
+;;;-------------------------------------------------------------------
 (ns sila-weka.core)
 
 (defn foo
