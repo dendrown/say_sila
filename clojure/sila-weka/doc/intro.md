e73e51ae645f8c3ed64a451ec55ca25a950c6421
den Drown <den@dendrown.net>
2017-07-19 22:53:45 -0400

    Skeleton for sila-weka in clojure

---
+++ clojure/sila-weka/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sila-weka
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure/sila-weka/test/sila_weka/core_test.clj
@@ -0,0 +1,7 @@
+(ns sila-weka.core-test
+  (:require [clojure.test :refer :all]
+            [sila-weka.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
