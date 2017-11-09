81a4011fc1f5f69e159ed85afbe5406b3c4da550
den Drown <den@dendrown.net>
2017-11-08 21:23:10 -0500

    Count retweet accounts (adhoc)

--- apps/say_sila/src/adhoc.erl
+++ apps/say_sila/src/adhoc.erl
@@ -18,26 +18,35 @@
 
 -include("twitter.hrl").
 
--define(OPTS, [{start, {2017, 10, 1}}]).
+-define(OPTS, [{start, {2017, 10, 1}},
+               {stop,  {2017, 11, 1}}]).
 
 md(Track, Bigs) ->
     P100 = fun(Full) ->
+        Tweets    = lists:filter(fun(#tweet{type=Type}) -> Type =/= retweet end, Full),
+        Retweets  = lists:filter(fun(#tweet{type=Type}) -> Type =:= retweet end, Full),
+        RePlayers = lists:foldl(fun(#tweet{rt_screen_name = RTSN}, Set) -> gb_sets:add_element(RTSN, Set) end,
+                                    gb_sets:new(),
+                                    Retweets),
         FLen = length(Full),
         {FLen,
-         100 * length(lists:filter(fun(#tweet{type=Type}) -> Type =/= retweet end, Full)) / FLen,
-         100 * length(lists:filter(fun(#tweet{type=Type}) -> Type =:= retweet end, Full)) / FLen}
+         gb_sets:size(RePlayers),
+         100 * length(Tweets)   / FLen,
+         100 * length(Retweets) / FLen}
         end,
     GetMD  = fun(N) ->
         Player  = lists:nth(N, Bigs),
         ScrName = Player#player.screen_name,
         Profile = io_lib:format("[~s](https://twitter.com/~s)", [ScrName, ScrName]),
         Tweets  = twitter:get_tweets(Track, Player, ?OPTS),
-        {Count, TT100, RT100} = P100(Tweets),
-        io:format("| ~-56s | ~4B | ~3B | ~3B |~n", [Profile,
-                                                    Count,
-                                                    round(TT100),
-                                                    round(RT100)])
+        {TweetCnt, RePlayerCnt, TT100, RT100} = P100(Tweets),
+        io:format("| ~-56s | ~4B | ~3B | ~3B   ~s |~n",
+                  [Profile,
+                   TweetCnt,
+                   round(TT100),
+                   round(RT100),
+                   if RePlayerCnt > 0 -> io_lib:format("(~3B)", [RePlayerCnt]); true -> "     " end])
         end,
-    io:format("| Screen Name                                              | Total| TT% | RT% |~n"),
-    io:format("| -------------------------------------------------------- | ----:| ---:| ---:|~n"),
+    io:format("| Screen Name                                              | Total| TT% | RT% (Accts) |~n"),
+    io:format("| -------------------------------------------------------- | ----:| ---:| -----------:|~n"),
     lists:foreach(GetMD, lists:seq(1, length(Bigs))).
