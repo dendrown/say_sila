ac8ce5ef8717bb8c92898bdb40bdfa0a0842f20d
den Drown <den@dendrown.net>
2018-02-05 20:02:29 -0500

    Generate ARFF for full raven report

--- apps/say_sila/src/weka.erl
+++ apps/say_sila/src/weka.erl
@@ -24,16 +24,89 @@
 
 -author("Dennis Drown <drown.dennis@courrier.uqam.ca>").
 
--export([tweets_to_arff/2]).
+-export([report_to_arff/2,
+         tweets_to_arff/2]).
 
 -include("sila.hrl").
 -include("llog.hrl").
+-include("raven.hrl").
 -include("twitter.hrl").
 
+-define(put_attr(FOut, Attrib, Type),   io:format(FOut, "@ATTRIBUTE ~s ~s\n",    [Attrib, Type])).
+-define(put_attr(FOut, A0, A1, Type),   io:format(FOut, "@ATTRIBUTE ~s_~s ~s\n", [A0, A1, Type])).
+-define(put_nl(FOut),                   io:put_chars(FOut, "\n")).
 
 
 %%====================================================================
 %% API
+%%--------------------------------------------------------------------
+-spec report_to_arff(Name   :: string(),
+                     RptMap :: map()) -> {ok, string()}
+                                       | {{error, term()}, string()}.
+%%
+% @doc  Generate an ARFF (Attribute-Relation File Format) relation for
+%       a Big/Regular player `raven' report.
+% @end  --
+report_to_arff(Name, #{big := BigRptPack,
+                       reg := RegRptPack}) ->
+    %
+    % TODO: (1) We need to handle full & tweet & retweet
+    %       (2) We'll want an ARFF per emotion
+    BigRpt = proplists:get_value(full, BigRptPack),
+    RegRpt = proplists:get_value(full, RegRptPack),
+
+    ?info("Converting report: rel[~s] cat[~s:~s] acct[~B:~B] tt[~B:~B]",
+          [Name,
+           BigRpt#report.category,      RegRpt#report.category,
+           BigRpt#report.num_players,   RegRpt#report.num_players,
+           BigRpt#report.num_tweets,    RegRpt#report.num_tweets]),
+
+    {FPath, FOut} = open_arff(Name),
+
+    ?put_attr(FOut, dts, string),
+    lists:foreach(fun(Var) ->
+                      ?put_attr(FOut, big, Var, numeric),
+                      ?put_attr(FOut, reg, Var, numeric)
+                      end,
+                  [cnt | ?EMOTIONS]),
+
+    % Create one data instance per day
+    io:put_chars(FOut, "\n@DATA\n"),
+    BigEmos = BigRpt#report.emotions,
+    RegEmos = RegRpt#report.emotions,
+    MakeInst = fun(Key, BigLot, {LotCnt, BigCnt, RegCnt}) ->
+        DTS = dts:str(Key),
+        case maps:get(Key, RegEmos, nak) of
+            nak ->
+                ?warning("No regular tweets on ~s", [DTS]),
+                {LotCnt + 1, BigCnt, RegCnt};
+            RegLot ->
+                BigLotCnt = BigLot#emotions.count,
+                RegLotCnt = RegLot#emotions.count,
+                io:format(FOut, "~s, ~B, ~B", [DTS, BigLotCnt, RegLotCnt]),
+                ?debug("BIG: ~p", [BigLot]),
+                ?debug("REG: ~p", [RegLot]),
+                lists:foreach(fun(Emo) ->
+                                  io:format(FOut, ", ~f, ~f",
+                                            [maps:get(Emo, BigLot#emotions.levels),
+                                             maps:get(Emo, RegLot#emotions.levels)])
+                                  end,
+                              ?EMOTIONS),
+                ?put_nl(FOut),
+                {LotCnt + 1,
+                 BigCnt + BigLotCnt,
+                 RegCnt + RegLotCnt}
+            end
+        end,
+    {LotCount,
+     BigTotal,
+     RegTotal} = maps:fold(MakeInst, {0, 0, 0}, BigEmos),
+
+    ?info("Processed ~B lots: big[~B] reg[~B]", [LotCount, BigTotal, RegTotal]),
+    close_arff(FPath, FOut).
+
+
+
 %%--------------------------------------------------------------------
 -spec tweets_to_arff(Name   :: string(),
                      Tweets :: [tweet()]) -> {ok, string()}
@@ -66,22 +139,19 @@
 % '''
 % @end  --
 tweets_to_arff(Name, Tweets) ->
-    FPath = make_fpath(Name),
-    {ok, FOut} = file:open(FPath, [write]),
+    %
+    {FPath, FOut} = open_arff(Name),
 
-    io:format(FOut, "@RELATION  ~s~n~n", [Name]),
-    io:put_chars(FOut, "@ATTRIBUTE id string\n"),
-    io:put_chars(FOut, "@ATTRIBUTE screen_name string\n"),
-    io:put_chars(FOut, "@ATTRIBUTE text string\n"),
+    ?put_attr(FOut, id,          string),
+    ?put_attr(FOut, screen_name, string),
+    ?put_attr(FOut, text,        string),
 
     % Fill in the tweet text as attribute data
     io:put_chars(FOut, "\n@DATA\n"),
     write_tweets(FOut, Tweets),
 
     % All ready for some learnin'
-    FStatus = file:close(FOut),
-    ?info("ARFF<create>: path[~s] stat[~p]", [FPath, FStatus]),
-    {FStatus, FPath}.
+    close_arff(FPath, FOut).
 
 
 
@@ -89,11 +159,40 @@ tweets_to_arff(Name, Tweets) ->
 %%====================================================================
 %% Internal functions
 %%--------------------------------------------------------------------
+-spec open_arff(Name :: string()) -> {string(), term()}.
+%%
+% @doc  Opens an ARFF file for writing the specified relation and
+%       returns the full pathname to the file and its file descriptor.
+% @end  --
+open_arff(Name) ->
+    %
+    FPath = make_fpath(Name),
+    {ok, FOut} = file:open(FPath, [write]),
+
+    io:format(FOut, "@RELATION  ~s~n~n", [Name]),
+    {FPath, FOut}.
+
+
+
+%%--------------------------------------------------------------------
+-spec close_arff(FPath  :: string(),
+                 FOut   :: term()) -> ok
+                                  | {error, term()}.
+%%
+% @doc  Closes an ARFF file, previously opened with `open_arff'.
+% @end  --
+close_arff(FPath, FOut) ->
+    FStatus = file:close(FOut),
+    ?info("ARFF<create>: path[~s] stat[~p]", [FPath, FStatus]),
+    {FStatus, FPath}.
+
+
+
 -spec make_fpath(Name :: string()) -> string().
 %%
 % @doc  Returns the full filepath for an ARFF file with the specified
-%       name.  This function also makes sure the subdirectory it is
-%       using under `/tmp' exists.
+%       name.  Note that the path must be valid for this function to
+%       succeed.
 % @end  --
 make_fpath(Name) ->
     FPath = lists:flatten(io_lib:format("~s/weka/~s.arff", [?WORK_DIR, Name])),
