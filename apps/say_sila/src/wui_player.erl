b01e846a28089c599be4a91aa07101e328a086dd
den Drown <den@dendrown.net>
2018-04-08 19:51:32 -0400

    Using bootstrap for play communications venn report

--- apps/say_sila/src/wui_player.erl
+++ apps/say_sila/src/wui_player.erl
@@ -36,6 +36,22 @@ out(Arg) ->
     Track  = case wui:get_track(Arg) of     % TODO: Handled returned binary
         _ -> gw
     end,
+
+    {ehtml, make_comms_venn(Track, P100)}.
+
+
+
+%%====================================================================
+%% Internal functions
+%%--------------------------------------------------------------------
+-spec make_comms_venn(Track :: atom(),
+                      P100  :: float()) -> {table,  list(), list()}.
+%%
+% @doc  Creates a Venn diagram and supporting table for the communication
+%       code combinations given a specific tracker and inclusion percentage.
+% @end  --
+make_comms_venn(Track, P100) ->
+    %
     Counts = player:get_big_venn(Track, P100),
     Rower  = fun({Comms, Cnt}) ->
                  CommsUp  = lists:map(fun(X) -> string:uppercase(atom_to_list(X)) end, Comms),
@@ -43,23 +59,24 @@ out(Arg) ->
                  CommsTxt = lists:join(", ", CommsUp),
                  {tr, [],
                       [{td, [?WUI_LEFT],                 CommsTxt},
-                       {td, [?WUI_RIGHT, {id, CommsID}], integer_to_binary(Cnt)}]}
+                       {td, [?WUI_RIGHT, {id, CommsID}], integer_to_binary(Cnt)},
+                       {td, [],                          <<"&nbsp;">>}]}
                  end,
 
-    {ehtml, {table, [],
-                    [{thead, [],
-                             [{th, [?WUI_CENTER], <<"Venn">>},
-                              {th, [?WUI_LEFT],   <<"Comm(s)">>},
-                              {th, [?WUI_CENTER], <<"Count">>}]},
-                     {tbody, [],
-                             [{td, [{id,      venn},
-                                    {width,   600},
-                                    {rowspan, integer_to_binary(1 + length(Counts))}],
-                                   <<"&nbsp;">>}
-                             | [Rower(CC) || CC <- Counts]]} ]}}.
-
-
-
-%%====================================================================
-%% Internal functions
-%%====================================================================
+    {table, [{class, <<"table table-sm table-hover">>}],
+            [{thead, [{class, <<"thead-dark">>}],
+                     [{th, [?WUI_CENTER,
+                            ?wui_cspan(4)],
+                           ?str_fmt("<big>~s @ ~B%</big>", [twitter:to_hashtag(Track),
+                                                            round(100 * P100)])}]},
+             {thead, [],
+                     [{th, [?WUI_CENTER], <<"Venn">>},
+                      {th, [?WUI_LEFT],   <<"Comm(s)">>},
+                      {th, [?WUI_RIGHT],  <<"Count">>},
+                      {th, [],            <<"&nbsp;">>}]},
+             {tbody, [],
+                     [{td, [{id,      venn},
+                            {width,   650},
+                            {rowspan, integer_to_binary(1 + length(Counts))}],
+                           <<"&nbsp;">>}
+                     | [Rower(CC) || CC <- Counts]]} ]}.
