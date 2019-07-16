aa6a2ea04290137812d2e3ea26ba0a6dcead42f6
den Drown <den@dendrown.net>
2019-07-16 16:01:08 -0400

    Utilities relating to social media

---
+++ apps/say_sila/priv/fnode/say/src/say/social.clj
@@ -0,0 +1,33 @@
+;;;; -------------------------------------------------------------------------
+;;;;
+;;;;        _/_/_/  _/_/_/  _/          _/_/
+;;;;     _/          _/    _/        _/    _/
+;;;;      _/_/      _/    _/        _/_/_/_/
+;;;;         _/    _/    _/        _/    _/
+;;;;  _/_/_/    _/_/_/  _/_/_/_/  _/    _/
+;;;;
+;;;; Utilities relating to social media
+;;;;
+;;;; @copyright 2019 Dennis Drown et l'Université du Québec à Montréal
+;;;; -------------------------------------------------------------------------
+(ns say.social
+  (:require [say.genie      :refer :all]
+            [say.log        :as log]
+            [clojure.string :as str]))
+
+
+;;; --------------------------------------------------------------------------
+(set! *warn-on-reflection* true)
+
+(def ^:const TOKEN-SPLITTER
+             #"_|\s|(?=\p{Lu}\p{Ll})|(?<=\p{Ll})(?=\p{Lu})|(?<=\p{L})(?=\p{N})|(?<=\p{N})(?=\p{L})")
+
+
+;;; --------------------------------------------------------------------------
+(defn tokenize
+  "
+  Attempts to split up a username or similar into component tokens.
+  "
+  [txt]
+  (filter seq (str/split txt TOKEN-SPLITTER)))
+
