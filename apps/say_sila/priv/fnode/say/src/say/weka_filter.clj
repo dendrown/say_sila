7a561359ae4e2b49d4bd0d83c1e81153e3317060
den Drown <den@dendrown.net>
2019-07-26 18:32:34 -0400

    Avoid reflection on self calls; wonky getCapabilities

--- apps/say_sila/priv/fnode/say/src/say/weka_filter.clj
+++ apps/say_sila/priv/fnode/say/src/say/weka_filter.clj
@@ -17,6 +17,8 @@
             [clojure.string  :as str])
   (:import  [affective.core ArffLexiconEvaluator]
             [weka.core Attribute
+                       Capabilities
+                       Capabilities$Capability
                        DenseInstance
                        Instance
                        Instances]
@@ -32,33 +34,45 @@
   Returns a Weka batch Filter to count female/male names in Instance Attributes.
   "
   []
-  (proxy [SimpleBatchFilter] []
+  (letfn [(^Instances shape-oinsts [^Instances iinsts]
+            (let [acnt (.numAttributes iinsts)
+                  attr (Attribute. "gender-name")]
+             ;;
+             ;; TODO: We'll need a better attr naming scheme
+             (doto (Instances. iinsts 0)
+                   (.insertAttributeAt attr acnt))))]
 
-    ;; -----------------------------------------------------------------------
-    (globalInfo []
-      "A Filter to count female/male names in Instance Attributes.")
-      
 
-    ;; -----------------------------------------------------------------------
-    (determineOutputFormat [^Instances iinsts]
-      (let [acnt (.numAttributes iinsts)
-            attr (Attribute. "gender-name")]
-        ;;
-        ;; TODO: We'll need a better attr naming scheme
-        (doto (Instances. iinsts 0)
-              (.insertAttributeAt attr acnt))))
+    (proxy [SimpleBatchFilter] []
+
+      ;; ---------------------------------------------------------------------
+      (globalInfo []
+        "A Filter to count female/male names in Instance Attributes.")
+
+
+      (determineOutputFormat [iinsts]
+        (shape-oinsts iinsts))
     
 
-    ;; -----------------------------------------------------------------------
-    (process [^Instances iinsts]
-      (let [oinsts  (Instances. ^Instances (.determineOutputFormat this iinsts) 0)
-            ocnt    (.numAttributes oinsts)
-            icnt    (.numAttributes iinsts)
-            fndx    (dec ocnt)]
-        (doseq [^Instance iinst (seq iinsts)]
-          (let [oattrs (double-array ocnt)]
-            (dotimes [i icnt]
-              (aset oattrs i (.value iinst i)))
-            (aset oattrs fndx 0.00)
-            (.add oinsts (DenseInstance. 1.0 oattrs))))
-        oinsts))))
+      (getCapabilities []
+        (let [^SimpleBatchFilter this this
+              ^Capabilities      caps (proxy-super getCapabilities)]
+          (doto caps
+                (.enableAllAttributes)
+                (.enableAllClasses)
+                (.enable Capabilities$Capability/NO_CLASS))))
+
+
+      (process [^Instances iinsts]
+        (let [oinsts  (Instances. ^Instances (shape-oinsts iinsts) 0)
+              ocnt    (.numAttributes oinsts)
+              icnt    (.numAttributes iinsts)
+              fndx    (dec ocnt)]
+          (doseq [^Instance iinst (seq iinsts)]
+            (let [oattrs (double-array ocnt)]
+              (dotimes [i icnt]
+                (aset oattrs i (.value iinst i)))
+              (aset oattrs fndx 0.00)
+              (.add oinsts (DenseInstance. 1.0 oattrs))))
+          oinsts)))))
+
