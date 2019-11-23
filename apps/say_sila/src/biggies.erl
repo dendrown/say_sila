7b7b3113e78edfeaa0a55c2ece7d92ab54e563a4
den Drown <den@dendrown.net>
2019-11-23 16:54:09 -0500

    Verify current results against previous runs

--- apps/say_sila/src/biggies.erl
+++ apps/say_sila/src/biggies.erl
@@ -18,13 +18,18 @@
          run_top_nn/2]).
 
 -include("sila.hrl").
+-include("emo.hrl").
 -include("player.hrl").
 -include("types.hrl").
 -include_lib("llog/include/llog.hrl").
 
+-type dataset() :: parms | train | test.
+
 
 %%--------------------------------------------------------------------
-%% Period shortcuts:
+-spec period(DataTag :: dataset()) -> proplist().
+%%
+%% Shortcut to get tracking run period.
 %%
 %% RUN-1: October 1, 2017 -- June 30, 2018              (10-fold CV)
 %%--------------------------------------------------------------------
@@ -76,7 +81,7 @@ run_top_nn_aux(Tracker, RunTag) ->
     sila:reset(),
     ok = raven:emote(Tracker, period(train)),
 
-    % We may still be waiting on messages from Wekaafter `emote' finishes
+    % We may still be waiting on messages from Weka after `emote' finishes
     Waiter = fun Recur() ->
         case raven:count_tweet_todo(Tracker) of
             0 -> ok;
@@ -96,5 +101,55 @@ run_top_nn_aux(Tracker, RunTag) ->
                  ?info("* ~s: ~B", [Comm, Cnt]) end,
              Totals),
 
-    influence:run_top_nn(Tracker, RunTag, oter, fear).
+    % Create the models
+    Results = influence:run_top_nn(Tracker, RunTag, oter, fear),
+    verify_run(Tracker, RunTag, train, oter, fear, Results),
+    Results.
 
+
+%%--------------------------------------------------------------------
+-spec verify_run(Tracker :: tracker(),
+                 RunTag  :: stringy(),
+                 DataTag :: dataset(),
+                 Comm    :: comm_code(),
+                 Emo     :: emotion(),
+                 Results :: proplist()) -> ack|nak|undefined.
+%%
+% @doc  Generates a hash representing the results of a given run.
+%       If we know what the results should be (we have a hash from
+%       a previous run), then we compare the current run's results
+%       and warn the user they don't match up.
+% @end  --
+verify_run(Tracker, RunTag, DataTag, Comm, Emo, Results) ->
+
+    Cfg = [Tracker, DataTag, Comm, Emo],
+    Key = crypto:hash(sha256, term_to_binary(Cfg)),
+    Val = crypto:hash(sha256, term_to_binary(Results)),
+
+    ?info("RUN: ~s % ~p: ~p => ~p", [RunTag, Cfg, Key, Val]),
+    case get_run_hash(Key) of
+        none -> ?notice("No previous results to compare"),                          undefined;
+        Val  -> ?notice("Results are consistent!"),                                 ack;
+        Prev -> ?warning("Results do not match previous runs: prev[~s]", [Prev]),   nak
+    end.
+
+
+
+%%--------------------------------------------------------------------
+-spec get_run_hash(Key :: binary()) -> none
+                                     | binary().
+%%
+% @doc  Returns the results hash from a previous run, or `none' if
+%       there is no recorded run for the specified key.
+%
+%       TODO: Later, we could have a better implementation with DETS.
+%             However, we are expecting a good bit of by-hand tinkering
+%             during this initial implementation.
+% @end  --
+get_run_hash(Key) ->
+    RunResults = #{% [gw,train,oter,fear]}).
+                   <<255,239,51,195,210,127,103,151,28,223,33,106,145,208,92,
+                     54,55,176,38,83,194,9,28,181,32,124,239,51,172,52,174,213>> =>
+                   <<87,81,48,74,205,149,219,162,115,48,33,140,69,53,160,50,
+                     153,189,181,58,63,125,49,50,106,163,246,21,227,160,53,54>>},
+    maps:get(Key, RunResults, none).
