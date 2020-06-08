e6c1af1dbae3f81741444f3c7421e93ce6589d53
den Drown <den@dendrown.net>
2020-06-07 22:06:30 -0400

    Test creating a sentiment example from a raw tweet

--- apps/say_sila/priv/fnode/say/test/say/senti_test.clj
+++ apps/say_sila/priv/fnode/say/test/say/senti_test.clj
@@ -2,11 +2,23 @@
     (:require [clojure.test   :refer :all]
               [say.senti      :refer :all]
               [say.log        :as log]
+              [weka.core      :as weka]
               [tawny.reasoner :as rsn]
               [tawny.fixture  :as fxt]))
 
 (def ^:const TEST-DATASET   "resources/test/test.A00.csv")
 (def ^:const GOLD-ARFF      "resources/test/test.A00.Sentiment140.GOLD.arff")
+
+(def ^:const GOLD-EXAMPLE   {:id        29923
+                             :polarity  :positive
+                             :content   '("@adamlefever" "Sen" "Thai" "let" "me" "down" "when" "I" "took"
+                                          "my" "parents" "there" "." "The" "food" "was" "not" "great" "."
+                                          "My" "faves" "are" "still" "Manee" "Thai" "and" "Thai" "Kitchen" ".")
+                             :pos-tags  '("@" "^" "^" "V" "O" "T" "R" "O" "V" "D" "N" "R" "," "D" "N" "V" "R"
+                                          "A" "," "D" "N" "V" "R" "^" "^" "&" "^" "^" ",")
+                             :rules     '(#{} #{} #{} #{} #{} #{} #{} #{} #{} #{} #{} #{} #{} #{}
+                                          #{"Joy" "Positive" "Trust"} #{} #{"NEGATION"} #{} #{} #{}
+                                          #{} #{} #{} #{} #{} #{} #{} #{} #{})})
 
 
 ;; ---------------------------------------------------------------------------
@@ -23,3 +35,11 @@
     (is (= cnt 1))
     (is (= (slurp arff)
            (slurp GOLD-ARFF)))))
+
+
+;; ---------------------------------------------------------------------------
+(deftest example
+  (let [insts (weka/load-arff GOLD-ARFF "sentiment")
+        xmps  (instances->examples insts)]
+    (is (= (first xmps)
+           GOLD-EXAMPLE))))
