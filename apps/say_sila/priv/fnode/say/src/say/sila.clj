9ea3054499fba504288681273fc1de93644b790d
den Drown <den@dendrown.net>
2020-10-19 18:46:50 -0400

    Tracking down funky quotes & Tweebo inconsistencies

--- apps/say_sila/priv/fnode/say/src/say/sila.clj
+++ apps/say_sila/priv/fnode/say/src/say/sila.clj
@@ -1453,7 +1453,7 @@
   (let [ont     (onter screen_name)
         include #(refine ont %1 :fact (is dul/hasComponent %2))
         equiv?  #(or (= %1 %2)
-                     (every? #{"\"" "QUOTE" "\"'"} [%1 %2]))
+                     (every? #{"\"" "QUOTE" "\"'" "'\""} [%1 %2]))
         make    (memoize (fn [ling n]
                             (let [tokid  (lbl/label-text-token tid n)
                                   token  (individual ont tokid)
