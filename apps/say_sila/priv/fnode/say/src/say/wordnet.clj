310f2777f3882466f712a9cbf490c6bcacfbce00
den Drown <den@dendrown.net>
2020-10-25 21:13:56 -0400

    Say-Sila synsets for cause/human/nature/conservation

--- apps/say_sila/priv/fnode/say/src/say/wordnet.clj
+++ apps/say_sila/priv/fnode/say/src/say/wordnet.clj
@@ -19,7 +19,28 @@
 
 (defonce Concept-Synsets
  ;; These are synsets applicable to climate change concepts as described in the Six Americas
- {"save"
+ {"cause"
+  #{"SID-07341157-N"    ;gloss "events that provide the generative force that is the origin of something..."
+    "SID-00007347-N"    ;gloss "any entity that produces an effect or is responsible for events or results"
+    "SID-01649143-V"}   ;gloss "give rise to; cause to happen or occur, not always intentionally..."
+
+  "human"
+  #{"SID-02474924-N"    ;gloss "any living or extinct member of the family Hominidae characterized by
+                        ;       superior intelligence, articulate speech, and erect carriage"
+    "SID-02754015-A"    ;gloss "characteristic of humanity; 'human nature'"
+    "SID-02754145-A"    ;gloss "relating to a person; 'the experiment was conducted on 6 monkeys and 2 human subjects'"
+    "SID-01261689-A"}   ;gloss "having human form or attributes as opposed to those of animals or divine beings..."
+
+  "nature"
+  #{"SID-09389659-N"}   ;gloss "the natural physical world including plants and animals and landscapes etc."
+
+
+ ;; --------------------------------------------------------------------------
+  "conservagtion"
+  #{"SID-07434199-N"    ;gloss "an occurrence of improvement by virtue of preventing loss or injury or other change"}
+    "SID-00820935-N"}   ;gloss "the preservation and careful management of the environment and of natural resources"}
+
+  "save"
   #{"SID-02556565-V"    ;gloss "save from ruin, destruction, or harm"
     "SID-02557529-V"    ;gloss "bring into safety; \"We pulled through most of the victims of the bomb attack\""
     "SID-02470006-V"}}) ;gloss "refrain from harming"
