39b388fe5aa06a11346b1201e39ce859735161dc
den Drown <den@dendrown.net>
2021-03-29 09:02:34 -0400

    Using B1-2019 dataset (min 5 tweets) for go! shortcut

--- apps/say_sila/priv/fnode/say/src/say/core.clj
+++ apps/say_sila/priv/fnode/say/src/say/core.clj
@@ -276,8 +276,8 @@
   [& opts]
   (log/notice "Minimum status count:" (cfg/?? :sila :min-statuses))
   (sila/create-world! :env
-                      "/srv/say_sila/weka/tweets/tweets.all.env.2019-Q4-2020-Q1Q2.T01.U01.arff"
-                      "/srv/say_sila/weka/tweets/tweets.all.env.2019-Q4-2020-Q1Q2.T01.S02.arff")
+                      "/srv/say_sila/weka/tweets/tweets.gw.b1.2019.min05.T01.U01.arff"
+                      "/srv/say_sila/weka/tweets/tweets.gw.b1.2019.min05.T01.S02.arff")
 
   ;; Do they want console and CSV reports??
   (doseq [[opt fun] [[:report   #(sila/report-world :no-pos)]
